51181d585921e74307c39e7e8b792d75eaefb661
vish2812 <mamdyalvishakha28@gmail.com>
2022-07-27 09:09:46 +0530

    Get Loan By ID API - Completed

--- src/main/java/com/barclays/homeloan/controller/LoanController.java
+++ src/main/java/com/barclays/homeloan/controller/LoanController.java
@@ -3,6 +3,14 @@ package com.barclays.homeloan.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.barclays.homeloan.repository.LoanRepository;
@@ -21,6 +29,19 @@ public class LoanController {
 	
 	@Autowired
 	LoanService loanService;
+	
+	@GetMapping(value = "/getLoanDetails/{id}")
+	public ResponseEntity<?> findLoanById(@PathVariable int id){
+		try {
+			logger.info("api running !!");
+			return new ResponseEntity<>(loanService.getLoanById(id), HttpStatus.OK);
+		}
+		catch(Exception e){
+			logger.error("Error occurred while fetching all Loan data: " + e.getMessage());
+            return new ResponseEntity<>("Error occurred while fetching all Loan data", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
 	
 	
 	
